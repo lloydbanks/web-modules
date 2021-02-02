654c6076fa3ebd3d10821e1dd54e899c229daa42
Dmitry Vyazitsky <dmitry.vyazitsky@homehero.co.il>
2021-02-02 10:40:04 +0300

    add inc

---
+++ inclinometry/component.6b010ecd625475e5b973.js
@@ -0,0 +1,117 @@
+(function(e, a) { for(var i in a) e[i] = a[i]; if(a.__esModule) Object.defineProperty(e, "__esModule", { value: true }); }(exports,
+    /******/ (() => { // webpackBootstrap
+      /******/ 	"use strict";
+      /******/ 	var __webpack_modules__ = ({
+
+        /***/ "./src/component/index.tsx":
+        /*!*********************************!*\
+          !*** ./src/component/index.tsx ***!
+          \*********************************/
+        /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+          __webpack_require__.r(__webpack_exports__);
+          /* harmony export */ __webpack_require__.d(__webpack_exports__, {
+            /* harmony export */   "default": () => /* binding */ RibbonInclinometry
+            /* harmony export */ });
+          /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+          /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+          var _jsxFileName = "C:\\dev\\grad-brd\\gpn-ds-grad-fe\\modules\\ribbon-modal\\src\\component\\index.tsx";
+
+          function RibbonInclinometry() {
+            return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("section", {
+              __self: this,
+              __source: {
+                fileName: _jsxFileName,
+                lineNumber: 6,
+                columnNumber: 7
+              }
+            }));
+          }
+
+          /***/ }),
+
+        /***/ "react":
+        /*!************************!*\
+          !*** external "react" ***!
+          \************************/
+        /***/ ((module) => {
+
+          module.exports = require("react");;
+
+          /***/ })
+
+        /******/ 	});
+      /************************************************************************/
+      /******/ 	// The module cache
+      /******/ 	var __webpack_module_cache__ = {};
+      /******/
+      /******/ 	// The require function
+      /******/ 	function __webpack_require__(moduleId) {
+        /******/ 		// Check if module is in cache
+        /******/ 		if(__webpack_module_cache__[moduleId]) {
+          /******/ 			return __webpack_module_cache__[moduleId].exports;
+          /******/ 		}
+        /******/ 		// Create a new module (and put it into the cache)
+        /******/ 		var module = __webpack_module_cache__[moduleId] = {
+          /******/ 			// no module.id needed
+          /******/ 			// no module.loaded needed
+          /******/ 			exports: {}
+          /******/ 		};
+        /******/
+        /******/ 		// Execute the module function
+        /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+        /******/
+        /******/ 		// Return the exports of the module
+        /******/ 		return module.exports;
+        /******/ 	}
+      /******/
+      /************************************************************************/
+      /******/ 	/* webpack/runtime/compat get default export */
+      /******/ 	(() => {
+        /******/ 		// getDefaultExport function for compatibility with non-harmony modules
+        /******/ 		__webpack_require__.n = (module) => {
+          /******/ 			var getter = module && module.__esModule ?
+              /******/ 				() => module['default'] :
+              /******/ 				() => module;
+          /******/ 			__webpack_require__.d(getter, { a: getter });
+          /******/ 			return getter;
+          /******/ 		};
+        /******/ 	})();
+      /******/
+      /******/ 	/* webpack/runtime/define property getters */
+      /******/ 	(() => {
+        /******/ 		// define getter functions for harmony exports
+        /******/ 		__webpack_require__.d = (exports, definition) => {
+          /******/ 			for(var key in definition) {
+            /******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+              /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+              /******/ 				}
+            /******/ 			}
+          /******/ 		};
+        /******/ 	})();
+      /******/
+      /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+      /******/ 	(() => {
+        /******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+        /******/ 	})();
+      /******/
+      /******/ 	/* webpack/runtime/make namespace object */
+      /******/ 	(() => {
+        /******/ 		// define __esModule on exports
+        /******/ 		__webpack_require__.r = (exports) => {
+          /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+            /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+            /******/ 			}
+          /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+          /******/ 		};
+        /******/ 	})();
+      /******/
+      /************************************************************************/
+      /******/ 	// module exports must be returned from runtime so entry inlining is disabled
+      /******/ 	// startup
+      /******/ 	// Load entry module and return exports
+      /******/ 	return __webpack_require__("./src/component/index.tsx");
+      /******/ })()
+
+));
+//# sourceMappingURL=component.11efaffbe71c3eb24c95.js.map
